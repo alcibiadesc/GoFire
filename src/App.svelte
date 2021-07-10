5d5012b8c623dabeb806ce430a965cbf0b8cf9a2
Alcibiades Cabral Diaz <acabdia@gmail.com>
2021-07-10 19:25:26 +0100

    refactor prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,22 @@
 <script>
-	import Home from "./lib/pages/Home.svelte";
-	import User from "./lib/pages/User.svelte";
-	import Menu from "./lib/components/Menu.svelte";
-	import Settings from "./lib/pages/Settings.svelte";
-	import Navbar from "./lib/components/Navbar.svelte";
-	let current = "home";
+  import Home from "./lib/pages/Home.svelte";
+  import User from "./lib/pages/User.svelte";
+  import Menu from "./lib/components/Menu.svelte";
+  import Settings from "./lib/pages/Settings.svelte";
+  import Navbar from "./lib/components/Navbar.svelte";
 
-	const selectedBtn = (icon) => {
-		current = icon;
-	};
+  let current = "home";
 
-	const pages = {
-		home: Home,
-		profile: User,
-		lightning: Settings,
-	};
-	$: page = pages[current];
+  const selectedBtn = (icon) => {
+    current = icon;
+  };
+
+  const pages = {
+    home: Home,
+    profile: User,
+    lightning: Settings,
+  };
+  $: page = pages[current];
 </script>
 
 <Navbar />
