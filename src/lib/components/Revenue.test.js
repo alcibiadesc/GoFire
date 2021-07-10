0581caff005ade7a470a918fdddf1122d53e34e0
Alcibiades Cabral Diaz <acabdia@gmail.com>
2021-07-10 11:19:29 +0100

    formatter

--- src/lib/components/Revenue.test.js
+++ src/lib/components/Revenue.test.js
@@ -1,32 +1,29 @@
 import { render, cleanup } from "@testing-library/svelte";
-
 import Revenue from "./Revenue.svelte";
 
 describe("Component | Revenue", () => {
-    let component;
-    let rendered;
+  let rendered;
+  let component;
 
-    beforeEach(() => {
-        const prop = { revenue: 52, percent: 0.53 }
-        rendered = render(Revenue, prop);
-        component = rendered.getByText("Beneficios");
-    });
+  beforeEach(() => {
+    const prop = { revenue: 52, percent: 0.53 };
+    rendered = render(Revenue, prop);
+  });
 
-    afterEach(cleanup);
+  afterEach(cleanup);
 
-    test("should render correctly", () => {
-        expect(component).toBeDefined();
-    });
+  test("should render correctly", () => {
+    component = rendered.getByText("Beneficios");
+    expect(component).toBeDefined();
+  });
 
+  test("show correctly the revenue", () => {
+    component = rendered.getByText("52 €");
+    expect(component).toBeDefined();
+  });
 
-    test("show correctly the revenue", () => {
-        component = rendered.getByText("52 €");
-        expect(component).toBeDefined();
-    })
-
-    test("show correctly the percent", () => {
-        component = rendered.getByText("53%");
-        expect(component).toBeDefined();
-    })
-
-})
+  test("show correctly the percent", () => {
+    component = rendered.getByText("53%");
+    expect(component).toBeDefined();
+  });
+});
