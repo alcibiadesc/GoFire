35085c3c6f39a70f037bc585b811956bdad3b46c
Alcibiades Cabral Diaz <acabdia@gmail.com>
2021-07-10 22:35:31 +0100

    refactor

--- src/lib/pages/Settings.svelte
+++ src/lib/pages/Settings.svelte
@@ -1,23 +1,23 @@
 <script>
-	import Button from "../atoms/Button.svelte";
-	import Card from "../atoms/Card.svelte";
-	import Input from "../atoms/Input.svelte";
-	import Tooltip from "../atoms/Tooltip.svelte";
-	import Grid from "../organisms/Grid.svelte";
-	import { data } from "./../../stores/data.js";
-	import { goal } from "./../../stores/goal.js";
+  import Button from "../atoms/Button.svelte";
+  import Card from "../atoms/Card.svelte";
+  import Input from "../atoms/Input.svelte";
+  import Tooltip from "../atoms/Tooltip.svelte";
+  import Grid from "../organisms/Grid.svelte";
+  import { data } from "./../../stores/data.js";
+  import { goal } from "./../../stores/goal.js";
 </script>
 
 <Card>
-	<h1>Opciones</h1>
-	<h3>Configura tus fuentes de ingresos</h3>
-	<Input label="Objetivo" type="text" bind:value={$goal} />
+  <h1>Edición</h1>
+  <h3>Configura tus fuentes de ingresos</h3>
+  <Input label="Objetivo" type="text" bind:value={$goal} />
 </Card>
 
-<Tooltip label="añadir nueva categoría " }>
-	<div>
-		<Button on:click={data.add} prop={{ icon: "add", style: "secondary" }} />
-	</div>
+<Tooltip label="añadir nueva categoría ">
+  <div>
+    <Button on:click={data.add} prop={{ icon: "add", style: "secondary" }} />
+  </div>
 </Tooltip>
 
 <Grid edit={true} />
