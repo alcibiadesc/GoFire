b61399dcec116b8fa98f45a6a1b8d844c8df50c6
Alcibiades Cabral Diaz <acabdia@gmail.com>
2021-07-10 22:16:24 +0100

    added test

---
+++ src/lib/pages/Home.test.js
@@ -0,0 +1,17 @@
+import { render, cleanup } from "@testing-library/svelte";
+
+import Home from "./Home";
+
+describe("Pages | Home", () => {
+  let component;
+  let rendered;
+
+  beforeEach(() => {
+    rendered = render(Home);
+    component = rendered.getByText("Hola crack");
+  });
+
+  test("should render correctly", () => {
+    expect(component).toBeDefined();
+  });
+});
