3e53bd2c7727cb821c3d91fd23140af65313d832
Alcibiades Cabral Diaz <acabdia@gmail.com>
2021-07-11 08:39:42 +0100

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'jest': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'rules': {
+  },
+};
