0924bc1a7f33d5d2fd3ab84b7dd34545b4c7ee54
Alcibiades Cabral Diaz <acabdia@gmail.com>
2021-07-11 14:57:36 +0100

    notification component

--- src/lib/pages/Notification.test.js
+++ src/lib/pages/Notification.test.js
@@ -6,7 +6,11 @@ describe('Pages | Notification', () => {
   let component;
   let rendered;
   beforeEach(() => {
-    rendered = render(Notification);
+    const array = {
+      saving: [{today: '1996-09-11', amount: 707}],
+      status: [{today: '2016-08-29', amount: 105}]};
+
+    rendered = render(Notification, {array});
     component = rendered.getByText('Movimientos');
   });
 
