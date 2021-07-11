3f9039bc782060fb6651273399950f701627d0bf
Alcibiades Cabral Diaz <acabdia@gmail.com>
2021-07-11 12:03:53 +0100

    added bell

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import Menu from "./lib/components/Menu.svelte";
   import Settings from "./lib/pages/Settings.svelte";
   import Navbar from "./lib/components/Navbar.svelte";
+  import Notification from "./lib/pages/Notification.svelte";
 
   let current = "home";
 
@@ -15,6 +16,7 @@
     home: Home,
     profile: User,
     lightning: Settings,
+    bell : Notification,
   };
 
   $: page = pages[current];
